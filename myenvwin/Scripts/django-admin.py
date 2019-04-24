1ff0fef19af5f4f88f8c416ab3c333a0b8bc8df9
yuzzzhakov <49301590+yuzzzhakov@users.noreply.github.com>
2019-04-24 08:28:48 +0300

    added myenvwin for windows

---
+++ myenvwin/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\andre\Desktop\MyPythonProjects\23april\myenvwin\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
